80b9b9370d02f070b229cd0f210fb3e683086997
sqdb304e <idwcorni@Harpy.localdomain>
2024-05-27 16:23:33 -0700

    Only updates when client is coonnected and server pushes chnages to client

--- start.js
+++ start.js
@@ -1,12 +1,24 @@
-import * as fs from "fs";
+import * as fs from 'fs';
 import express from 'express';
-import * as child_process from "child_process";
+import * as http from 'http'
+import * as child_process from 'child_process';
+import {Server} from 'socket.io';
 
 const app = express();
+const server = http.createServer(app);
+var io = new Server(server);
 
 const datafile = "new.h5"
 const imagefile = "new.jpeg"
 const PORT = 8080
+var should_render_on_connect = false;
+
+function render(){
+  const pythonProcess = child_process.spawn('python3',["renderH5.py",datafile]);
+  pythonProcess.on('exit', function() {
+    io.emit('update', true);
+  })
+}
 
 app.get('/', function(req, res){
   fs.readFile('index.html', function(err, data) {
@@ -24,13 +36,25 @@ app.get('/image', (req, res) => {
 var fsTimeout
 
 fs.watch(datafile, (eventType, filename) => {
-  if (!fsTimeout) {
-    console.log("Rendering New File")
-    const pythonProcess = child_process.spawn('python3',["renderH5.py",datafile]);
-    fsTimeout = setTimeout(function() { fsTimeout=null }, 2000)
+  if (fsTimeout) {
+    return;
+  }
+
+  if(io.of("/").sockets.size==0){
+    should_render_on_connect=true;
+    return;
+  }
+  render();
+  fsTimeout = setTimeout(function() { fsTimeout=null }, 2000)
+});
+
+io.on('connect', function(){
+  if(should_render_on_connect){
+    should_render_on_connect=false;
+    render();
   }
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('Server is running on port 8080');
 });
